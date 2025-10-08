fc0e5c83cc7ab47e548def73189884a7279441c2
art4iba2 <99066833+art4iba2@users.noreply.github.com>
2025-10-08 18:20:22 +0400

    Update task_6.py

--- task_6.py
+++ task_6.py
@@ -5,11 +5,14 @@ a = int(input())
 try:
     print(square_num(a))
 except NameError:
-    print("name 'math' is not defined. Did you forget to import 'math'? Joke, I import it for u")
+    print("name 'math' is not defined. Did you forget to import 'math'?")
+    print("Importing math module...")
     import math
+    print("Math module imported.")
     try:
         print(square_num(a))
     except ValueError:
         print("Wrong number for sqrt")
+
 
 
